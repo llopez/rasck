98d4cd8782395d2f5ae048d028a3fac426605f4d
Luis Lopez <luis.lopez@altoros.com>
2018-05-22 18:15:15 -0300

    wip

--- lib/rasck.rb
+++ lib/rasck.rb
@@ -9,11 +9,17 @@ module Rasck
     attr_accessor :config
 
     def run_checks
+      # result = {}
+      # config.custom_checks.each do |k, v|
+      #   result[k] = v.call(self)
+      # end
+
       all_checks.inject({}) do |m, v|
         case v
         when 'redis'
           m[v] = Rasck::Checks::Redis.check if defined?(::Redis)
         when 's3'
+          m[]
           m[v] = Rasck::Checks::S3.check if defined?(::Aws)
         else
           m[v] = config.custom_checks[v].call(self)
